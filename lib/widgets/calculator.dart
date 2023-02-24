54f1db2264f1321c9ebf67aa33aca0e6bf132f58
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2023-02-24 16:25:53 +0200

    Calories saved to db

--- lib/widgets/calculator.dart
+++ lib/widgets/calculator.dart
@@ -1,137 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class _WeightInput extends StatelessWidget {
-  @override
-  const _WeightInput({required this.controller});
-  final TextEditingController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      maxLines: 1,
-      maxLength: 5,
-      keyboardType: TextInputType.number,
-      inputFormatters: [
-        FilteringTextInputFormatter.digitsOnly,
-      ],
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Please enter your weight';
-        }
-        return null;
-      },
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        label: Text("Weight"),
-        helperText: "Input your weight in kg",
-      ),
-    );
-  }
-}
-
 enum Genders { male, female }
-
-class _GenderInput extends StatelessWidget {
-  @override
-  const _GenderInput({required this.gender, required this.updateGender});
-  final Genders? gender;
-  final void Function(Genders?) updateGender;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Column(
-        children: [
-          Text(
-            "Select your biological sex",
-            style: Theme.of(context).textTheme.titleMedium,
-            textAlign: TextAlign.left,
-          ),
-          RadioListTile(
-            title: const Text("Male"),
-            value: Genders.male,
-            groupValue: gender,
-            onChanged: updateGender,
-          ),
-          RadioListTile(
-            title: const Text("Female"),
-            value: Genders.female,
-            groupValue: gender,
-            onChanged: updateGender,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _IntensityDropdown extends StatelessWidget {
-  final double? intensity;
-  final void Function(double?) updateIntensity;
-  final Map intensities = const <String, double?>{
-    // "Select": 0.0,
-    "Light": 1.3,
-    "Usual": 1.5,
-    "Moderate": 1.7,
-    "Hard": 2.0,
-    "Very Hard": 2.2,
-  };
-
-  @override
-  const _IntensityDropdown(
-      {required this.intensity, required this.updateIntensity});
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButtonFormField<double>(
-      items: intensities.entries.map((entry) {
-        return DropdownMenuItem<double>(
-            value: entry.value, child: Text(entry.key));
-      }).toList(),
-      value: intensity,
-      onChanged: updateIntensity,
-      hint: const Text("Select your exersising intensity"),
-      validator: (value) {
-        if (value == 0 || value == null) {
-          return "Select intensity";
-        }
-        return null;
-      },
-    );
-  }
-}
-
-class _Calculate extends StatelessWidget {
-  final void Function() onPressed;
-  final int result;
-
-  @override
-  const _Calculate({
-    required this.onPressed,
-    required this.result,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        OutlinedButton(
-          onPressed: onPressed,
-          child: const Text(
-            "Calculate",
-          ),
-        ),
-        Text(
-          result.toString(),
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-      ],
-    );
-  }
-}
 
 class Caloriemeter extends StatefulWidget {
   @override
@@ -147,6 +19,12 @@ class _CaloriemeterState extends State<Caloriemeter> {
   Genders sex = Genders.male;
   double? intensity;
   int result = 0;
+
+  @override
+  void initState() {
+    _getResultFromDb();
+    super.initState();
+  }
 
   void updateResult(int newResult) {
     setState(() {
@@ -187,15 +65,132 @@ class _CaloriemeterState extends State<Caloriemeter> {
       key: _formKey,
       child: Column(
         children: [
-          _WeightInput(controller: weightController),
-          _GenderInput(gender: sex, updateGender: updateSex),
-          _IntensityDropdown(
-            intensity: intensity,
-            updateIntensity: updateIntensity,
+          _weightInput(),
+          _genderInput(),
+          _intensityDropdown(),
+          _calculate(),
+          _saveIntoDb(),
+        ],
+      ),
+    );
+  }
+
+  Future _saveToDb() async {
+    final user = FirebaseAuth.instance.currentUser!;
+    final userDoc =
+        FirebaseFirestore.instance.collection('users').doc(user.email);
+    final data = {'calories': result};
+    await userDoc.set(data);
+  }
+
+  Future _getResultFromDb() async {
+    final user = FirebaseAuth.instance.currentUser!;
+    final userDoc =
+        FirebaseFirestore.instance.collection('users').doc(user.email);
+    final data = await userDoc.get();
+    updateResult(data['calories'] ?? 0);
+  }
+
+  Widget _saveIntoDb() {
+    return ElevatedButton(
+      onPressed: _saveToDb,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      child: const Text(
+        'Remember',
+        style: TextStyle(color: Colors.white, fontSize: 28),
+      ),
+    );
+  }
+
+  Widget _calculate() {
+    return Column(
+      children: [
+        OutlinedButton(
+          onPressed: valAndCalc,
+          child: const Text(
+            "Calculate",
+            style: TextStyle(fontSize: 28),
           ),
-          _Calculate(
-            onPressed: valAndCalc,
-            result: result,
+        ),
+        Text(
+          result.toString(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ],
+    );
+  }
+
+  Widget _intensityDropdown() {
+    const Map intensities = <String, double?>{
+      "Light": 1.3,
+      "Usual": 1.5,
+      "Moderate": 1.7,
+      "Hard": 2.0,
+      "Very Hard": 2.2,
+    };
+    return DropdownButtonFormField<double>(
+      items: intensities.entries.map((entry) {
+        return DropdownMenuItem<double>(
+            value: entry.value, child: Text(entry.key));
+      }).toList(),
+      value: intensity,
+      onChanged: updateIntensity,
+      hint: const Text("Select your exersising intensity"),
+      validator: (value) {
+        if (value == 0 || value == null) {
+          return "Select intensity";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _weightInput() {
+    return TextFormField(
+      controller: weightController,
+      maxLines: 1,
+      maxLength: 5,
+      keyboardType: TextInputType.number,
+      inputFormatters: [
+        FilteringTextInputFormatter.digitsOnly,
+      ],
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your weight';
+        }
+        return null;
+      },
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        label: Text("Weight"),
+        helperText: "Input your weight in kg",
+      ),
+    );
+  }
+
+  Widget _genderInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Column(
+        children: [
+          Text(
+            "Select your biological sex",
+            style: Theme.of(context).textTheme.titleMedium,
+            textAlign: TextAlign.left,
+          ),
+          RadioListTile(
+            title: const Text("Male"),
+            value: Genders.male,
+            groupValue: sex,
+            onChanged: updateSex,
+          ),
+          RadioListTile(
+            title: const Text("Female"),
+            value: Genders.female,
+            groupValue: sex,
+            onChanged: updateSex,
           ),
         ],
       ),
